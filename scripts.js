7e0c943cbeb989673d6e4b1e626ff0f712d2592d
JesuisDv <jdfrancov2020@gmail.com>
2025-05-20 17:42:07 -0500

    Mejorar la interfaz y funcionalidad de carga de datos

--- scripts.js
+++ scripts.js
@@ -6,5 +6,21 @@ const cancelarBtn = document.querySelector("cancelarCarga");
 const estado = document.querySelector(".status");
 
 cargarBtn.onclick = () =>{
-    estado.style.display = flex
-};
+    estado.style.display = flex;
+    cancelarCarga.disable = false;
+
+new Promise((resolve, reject) =>{
+    rejectPromise = reject;
+    timeoutId = setTimeout(() =>{
+        resolve("Carga de datos completada")
+    },3000)
+});
+    .then((msg) =>{
+        estado.style.display = hidden;
+        cancelarCarga.disable = true;
+    });
+    .catch(() =>{
+        estado.textContent = 'Carga de datos rechazada X';
+        cancelarBtn.display = true;
+    });
+};  
