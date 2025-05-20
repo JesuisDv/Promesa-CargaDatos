d68c84e1b0e6f0fb525ff78c8c1ee0fe83c44780
JevuisDv <jdfrancov2020@gmail.com>
2025-05-20 16:55:29 -0500

    Primera carga de promesas

---
+++ scripts.js
@@ -0,0 +1,10 @@
+let timeoutId;
+let rejectPromise;
+
+const cargarBtn = document.querySelector(".cargar");
+const cancelarBtn = document.querySelector("cancelarCarga");
+const estado = document.querySelector(".status");
+
+cargarBtn.onclick = () =>{
+    estado.style.display = flex
+};
